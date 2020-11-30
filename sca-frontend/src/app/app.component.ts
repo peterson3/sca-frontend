0f77bad6673db1024621962bbdde9f3eef74b5b0
unknown <tiago.peterson@gmail.com>
2020-11-30 13:22:22 -0300

    Adicionando log do Env

--- sca-frontend/src/app/app.component.ts
+++ sca-frontend/src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, isDevMode, OnInit } from '@angular/core';
 import { LocationStrategy, PlatformLocation, Location } from '@angular/common';
+import { environment } from 'environments/environment';
 
 @Component({
   selector: 'app-root',
@@ -11,7 +12,13 @@ export class AppComponent implements OnInit {
      constructor(public location: Location) {}
 
     ngOnInit(){
+      if (isDevMode()){
+        console.log("Desenvolvimento", environment.baseUrl);
     }
+    else{
+      console.log("Produção", environment.baseUrl);
+    }
+  }
 
     isMap(path){
       var titlee = this.location.prepareExternalUrl(this.location.path());
